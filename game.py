278a41e2abdf5138580d5da7dc97e7eeb972042e
Alireza <alireza.niary@gmail.com>
2020-11-13 20:36:16 +0330

    seperating game logic in game class

---
+++ game.py
@@ -0,0 +1,18 @@
+class Game:
+    def __init__(self, players, board, pieces):
+        self.players = players
+        self.board = board
+        self.pieces = pieces
+
+    def logic(self, team, rolled_number):
+        if rolled_number == 6:
+            repeat = True
+
+
+    def detect_possible_moves(self):
+        pass
+
+    def calculate_new_position(self):
+        pass
+
+
